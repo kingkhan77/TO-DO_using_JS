6906208e8bbf8976864bf41a8bb3ef14beee467f
kingkhan77 <shahzad.2feb@gmail.com>
2024-06-25 11:54:54 +0530

    Added mark a done

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 var StoredTodoString = localStorage.getItem("todos");
 //Get the stored todos string from local storage
 
-let todos = [];
+var todosArray = JSON.parse(StoredTodoString) || [];
 
-todos = JSON.parse(StoredTodoString) || [];
-//parse the string to get the actual stored todos array
-// OR DIRECTLY ---- var todos = JSON.parse(localStorage.getItem("todos")) || [] ;
-
-if(todos.length > 0){
-    todos.forEach(task => {
-        addTOList(task);
+if (todosArray.length > 0) {
+    todosArray.forEach(task => {
+        addToList(task);
     });
 }
 
- document.getElementById("submit").addEventListener('click',
-    function(event){
+document.getElementById("submit").addEventListener('click',
+    function (event) {
         event.preventDefault();
 
         const task = document.getElementById("task").value.trim();
-        console.log(typeof task);
-        if(task){
-            todos.push(task);
-            console.log(todos);
-            localStorage.setItem('todos', JSON.stringify(todos));
-            addTOList(task);
+
+        if (task) {
+            todosArray.push(task);
+            localStorage.setItem('todos', JSON.stringify(todosArray));
+            addToList(task);
             document.getElementById('task').value = '';
         }
     }
 );
 
-console.log(typeof todos);
+// console.log(typeof todosArray);
 
-function addTOList(new_task){
+function addToList(new_task) {
     const li = document.createElement('li');
     li.textContent = new_task;
-    
+
     document.getElementById('list').appendChild(li);
     //add new li to ul
 }
+
+document.querySelectorAll('li').forEach(
+    function (li) {
+        li.addEventListener('click', function () {
+            this.style.textDecoration = 'line-through';
+        })
+    }
+);
+
+document.querySelectorAll('li').forEach(
+    function (li, i) {
+        li.addEventListener('click', function (i) {
+            todosArray.splice(i,1,);
+            localStorage.setItem('todos', JSON.stringify(todosArray));
+        });
+    }
+);
+
