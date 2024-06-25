7bc07d5fcbecfba1f782395bc2ce8947acf8bbd8
kingkhan77 <shahzad.2feb@gmail.com>
2024-06-25 13:48:52 +0530

    updated

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var StoredTodoString = localStorage.getItem("todos");
 
 var todosArray = JSON.parse(StoredTodoString) || [];
 
-if (todosArray.length > 0) {
-    todosArray.forEach(task => {
-        addToList(task);
-    });
+renderTodoList();
+
+function renderTodoList() {
+    document.getElementById('list').innerHTML = '';
+    if (todosArray.length > 0) {
+        todosArray.forEach((task, index) => {
+            addToList(index, task);
+        });
+    }
 }
+
 
 document.getElementById("submit").addEventListener('click',
     function (event) {
@@ -17,8 +23,9 @@ document.getElementById("submit").addEventListener('click',
 
         if (task) {
             todosArray.push(task);
+            console.log(todosArray);
             localStorage.setItem('todos', JSON.stringify(todosArray));
-            addToList(task);
+            renderTodoList();
             document.getElementById('task').value = '';
         }
     }
@@ -26,28 +33,30 @@ document.getElementById("submit").addEventListener('click',
 
 // console.log(typeof todosArray);
 
-function addToList(new_task) {
+function addToList(index, new_task) {
     const li = document.createElement('li');
     li.textContent = new_task;
-
+    li.setAttribute('onclick', 'deleteTask(' + index + ')')
+    const done = document.createElement('button');
+    done.className = 'mark_as_done';
+    done.textContent = '✔';
+    li.appendChild(done);
     document.getElementById('list').appendChild(li);
     //add new li to ul
 }
 
-document.querySelectorAll('li').forEach(
-    function (li) {
-        li.addEventListener('click', function () {
-            this.style.textDecoration = 'line-through';
+document.querySelectorAll('.mark_as_done').forEach(
+    function (button) {
+        button.addEventListener('click', function (e) {
+            e.stopPropagation();
+            this.parentNode.style.textDecoration = 'line-through';
         })
     }
 );
 
-document.querySelectorAll('li').forEach(
-    function (li, i) {
-        li.addEventListener('click', function (i) {
-            todosArray.splice(i,1,);
-            localStorage.setItem('todos', JSON.stringify(todosArray));
-        });
-    }
-);
 
+function deleteTask(taskIndex) {
+    todosArray.splice(taskIndex, 1);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    renderTodoList();
+}
