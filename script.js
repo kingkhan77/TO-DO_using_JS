f26c8ed1963ececb81d91cd4134f94c281a078e3
kingkhan77 <shahzad.2feb@gmail.com>
2024-06-26 07:39:33 +0530

    CSS improved

--- script.js
+++ script.js
@@ -31,29 +31,50 @@ document.getElementById("submit").addEventListener('click',
     }
 );
 
-// console.log(typeof todosArray);
 
 function addToList(index, new_task) {
     const li = document.createElement('li');
     li.textContent = new_task;
-    li.setAttribute('onclick', 'deleteTask(' + index + ')')
+    // li.setAttribute('onclick', 'deleteTask(' + index + ')')
+
     const done = document.createElement('button');
     done.className = 'mark_as_done';
     done.textContent = '✔';
+
+    done.setAttribute('onclick',
+        `event.stopPropagation();
+        this.parentNode.style.textDecoration = 'line-through';`
+        // localStorage.setItem('todos', JSON.stringify(todosArray));
+        // renderTodoList();` 
+    );
+
+    const dlt = document.createElement('button');
+    dlt.className = 'delete';
+    dlt.textContent = '✘';
+    dlt.setAttribute('onclick', `deleteTask(${index})`);
+
     li.appendChild(done);
+    li.appendChild(dlt);
+
     document.getElementById('list').appendChild(li);
     //add new li to ul
 }
 
-document.querySelectorAll('.mark_as_done').forEach(
-    function (button) {
-        button.addEventListener('click', function (e) {
-            e.stopPropagation();
-            this.parentNode.style.textDecoration = 'line-through';
-        })
-    }
-);
+// document.querySelectorAll('.mark_as_done').forEach(
+//     function (button) {
+//         button.addEventListener('click', function (e) {
+//             e.stopPropagation();
+//             this.parentNode.style.textDecoration = 'line-through';
+//         })
+//     }
+// );
 
+function markAsDoneTask(e) {
+    // e.stopPropagation();
+    this.parentNode.style.textDecoration = 'line-through';
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    renderTodoList();
+}
 
 function deleteTask(taskIndex) {
     todosArray.splice(taskIndex, 1);
